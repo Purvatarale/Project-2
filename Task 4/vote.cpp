950f22b9989504867e9918d151b7e9e580a0d6d1
Purvatarale <78158135+Purvatarale@users.noreply.github.com>
2021-01-30 14:00:18 +0530

    Add files via upload
    This is a C++ program where we check if a person is eligible to vote or not.

---
+++ Task 4/vote.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	
+	int x;
+	cout<<"MayI know your age?";
+	cin>>x;
+	if (x>=18)
+	cout<<"You are eligible to vote.";
+	
+    else
+	{
+		cout<<"Sorry!You are to young to vote.";
+	}
+	 return 0;
+}
+
